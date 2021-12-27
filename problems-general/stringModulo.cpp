9761dc60dc791b92e777b1dd0238da9172e2fa14
Sharikzama <sharik.zama1504777@gmail.com>
2021-12-27 13:00:39 -0500

    Create stringModulo.cpp

---
+++ problems-general/stringModulo.cpp
@@ -0,0 +1,23 @@
+// Given 2 numbers N & P. Print N % P.
+
+// 1 <= N <= 1010000
+// 1 <= P <= 1015
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string n;
+    int ans=0;
+    long long p;
+    
+    cin>>n;
+    cin>>p;
+    for (int i = 0; i < n.length(); i++) {
+        ans=((ans*10)+(n[i]-'0'))%p;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
